2b6b6ecf3dfd4d57d92cd6962609a15841d33dd5
geonheegit <geonhee625@gmail.com>
2022-03-27 01:32:16 +0900

    로컬경로 지정
    절대경로에서 로컬경로로 바꿈

--- player.py
+++ player.py
@@ -4,7 +4,7 @@ import settings
 class Player(pygame.sprite.Sprite):
 	def __init__(self,pos,groups, obstacle_sprites):
 		super().__init__(groups)
-		self.image = pygame.image.load('C:/Users/gram/PycharmProjects/test_game/graphics/test/player.png').convert_alpha()
+		self.image = pygame.image.load('graphics/test/player.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft = pos)
 
 		# 추가
