331565809504f7bb035a4a67ae1d52a18f5439fd
geonheegit <geonhee625@gmail.com>
2022-05-01 01:18:15 +0900

    택스처 업데이트 + 나무 배치 시험

--- level.py
+++ level.py
@@ -1,6 +1,7 @@
 import pygame 
 from settings import *
 from tile import Tile
+from objects import Tree
 from player import Player
 from bg_map import Map
 
@@ -29,6 +30,8 @@ class Level:
 					Tile((x,y),[self.visible_sprites, self.obstacle_sprites]) # visible_sprites & obstacle_sprites에 포함
 				if col == 'p':
 					self.player = Player((x,y),[self.visible_sprites], self.obstacle_sprites) # visible_sprites에 포함 / obstacle_sprites 그룹 제공
+				if col == 't':
+					Tree((x,y),[self.visible_sprites, self.obstacle_sprites]) # visible_sprites & obstacle_sprites에 포함
 
 	def run(self): # main에서 무한 반복
 		# 화면에 출력
---
+++ objects.py
@@ -0,0 +1,11 @@
+import pygame
+from settings import *
+
+class Tree(pygame.sprite.Sprite):
+	def __init__(self, pos, groups):
+		super().__init__(groups)
+		self.image = pygame.image.load("graphics/test/blue_big_tree.png").convert_alpha()
+		self.image_size = self.image.get_size()
+		self.image = pygame.transform.scale(self.image, (self.image_size[0] * 2, self.image_size[1] * 2))
+		self.rect = self.image.get_rect(topleft = pos)
+		self.hitbox = self.rect.inflate(-120, -180)
