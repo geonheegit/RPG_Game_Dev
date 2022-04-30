9f38bbd8b1edc7fab65da906dc19c33f5aeee244
geonheegit <geonhee625@gmail.com>
2022-04-30 17:19:04 +0900

    배경구현 편하게 (테스트 보완)

--- bg_map.py
+++ bg_map.py
@@ -3,6 +3,9 @@ import pygame
 class Map(pygame.sprite.Sprite):
     def __init__(self, groups):
         super().__init__(groups)
+        self.zoom = 2
         self.image = pygame.image.load("map/testmap.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (3200, 3200))
+        self.image_size = self.image.get_size()
+        self.image = pygame.transform.scale(self.image, (self.image_size[0] * self.zoom, self.image_size[1] * self.zoom)) # 32픽셀맵 :1600 더블
         self.rect = self.image.get_rect()
+        print(self.image.get_size())
