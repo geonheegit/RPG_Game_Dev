2342cd7faf2438b86f3aaf80ff9944918c1d4d47
geonheegit <geonhee625@gmail.com>
2022-07-14 01:13:00 +0900

    도움말 기능 추가, 미니맵 m키 토글 기능 추가

--- main.py
+++ main.py
@@ -17,8 +17,12 @@ FPS = settings.FPS
 
 screen = pygame.display.set_mode((WIDTH, HEIGTH))
 
-minimap_island = pygame.image.load("map/minimap_island.png").convert()
+minimap_island = pygame.image.load("map/minimap_island.png").convert_alpha()
+minimap_cave = pygame.image.load("map/minimap_cave.png").convert_alpha()
 minimap_on = False
+minimap_posx = 784
+
+question_mark_icon = pygame.image.load("graphics/icon/question_mark.png").convert_alpha()
 
 def update_position():
     # 플레이어 위치 표시
@@ -31,6 +35,16 @@ def update_fps():
     fps = str(int(clock.get_fps()))
     fps_text = font.render("FPS: " + fps, 1, pygame.Color("white"))
     return fps_text
+
+def question_mark():
+    screen.blit(question_mark_icon, (10, 100))
+    if 10 <= pygame.mouse.get_pos()[0] <= 60 and 100 <= pygame.mouse.get_pos()[1] <= 150:
+        if game_state.STAGE == 'cave':
+            screen.blit(kr_font.render("m키를 눌러서 미니맵을 켜고 끌 수 있습니다.", 1,
+                                        pygame.Color("white")), (WIDTH / 4 + 70, 100))
+        else:
+            screen.blit(kr_font.render("m키를 눌러서 미니맵을 켜고 끌 수 있습니다.", 1,
+                                       pygame.Color("black")), (WIDTH / 4 + 70, 100))
 
 class GameState():
     def __init__(self):
@@ -80,11 +94,20 @@ class GameState():
                                 pygame.Color("white")), (WIDTH / 4 + 70, HEIGTH / 2))
 
     def island(self):
+        global minimap_on
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+
             if event.type == pygame.KEYDOWN:
+                # 미니맵 토글
+                if event.key == pygame.K_m:
+                    if not minimap_on:
+                        minimap_on = True
+                    else:
+                        minimap_on = False
+                # 맵 이동 트리거
                 if event.key == pygame.K_SPACE:
                     # 동굴 x 1900 - 2000, y 2300 - 2400 / 숲 x 2400 - 2500, y 600 - 700 / 해변 x 600 - 800, y 2800 - 2900
                     if 1900 < level.player.hitbox.x < 2000 and 2300 < level.player.hitbox.y < 2400:
@@ -123,16 +146,28 @@ class GameState():
         screen.fill('black')
         level.run()
         screen.blit(update_fps(), (10, 0))
+        question_mark()
+
+        if minimap_on:
+            if self.STAGE == 'island' and level.current_stage == 'island':
+                screen.blit(minimap_island, (minimap_posx, 0))
 
         # 플레이어 위치 표시
         update_position()
 
     def cave(self):
+        global minimap_on
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
+                # 미니맵 토글
+                if event.key == pygame.K_m:
+                    if not minimap_on:
+                        minimap_on = True
+                    else:
+                        minimap_on = False
                 if event.key == pygame.K_SPACE:
                     # 동굴 x 1900 - 2000, y 2300 - 2400 / 숲 x 2400 - 2500, y 600 - 700 / 해변 x 600 - 800, y 2800 - 2900
                     if 800 < level.player.hitbox.x < 900 and 3100 < level.player.hitbox.y < 3200:
@@ -161,6 +196,12 @@ class GameState():
         screen.fill('black')
         level.run()
         screen.blit(update_fps(), (10, 0))
+        question_mark()
+
+        if minimap_on:
+            if self.STAGE == 'cave' and level.current_stage == 'cave':
+                screen.blit(minimap_cave, (minimap_posx, 0))
+
         # 플레이어 위치 표시
         update_position()
 
@@ -200,6 +241,7 @@ class GameState():
         screen.fill('black')
         level.run()
         screen.blit(update_fps(), (10, 0))
+        question_mark()
 
         # 플레이어 위치 표시
         update_position()
@@ -240,6 +282,7 @@ class GameState():
         screen.fill('black')
         level.run()
         screen.blit(update_fps(), (10, 0))
+        question_mark()
 
         # 플레이어 위치 표시
         update_position()
@@ -289,21 +332,6 @@ class GameState():
                 screen.blit(kr_font.render("무인도로 다시 돌아가려면 스페이스바를 누르세요.", 1,
                                            pygame.Color("black")), (WIDTH / 4 + 70, 100))
 
-    # def minimap_loader(self):
-    #     global minimap_on
-    #     for event in pygame.event.get():
-    #         if event.type == pygame.KEYDOWN:
-    #             print("qwd")
-    #             if event.key == pygame.K_m:
-    #                 if not minimap_on:
-    #                     minimap_on = True
-    #                 else:
-    #                     minimap_on = False
-
-        if minimap_on:
-            if self.STAGE == 'island' and level.current_stage == 'island':
-                screen.blit(minimap_island, (0, 0))
-
     def bgm_play(self):
         if self.is_cave and not self.cave_bgm_chan.get_busy():
             self.island_bgm_chan.stop()  # 중복 재생 방지
@@ -317,8 +345,8 @@ game_state = GameState()
 
 while True:
     game_state.state_manager()
-    game_state.minimap_loader()
     game_state.text_renderer()
+
     clock.tick(FPS)
     pygame.display.update()
     game_state.bgm_play()
