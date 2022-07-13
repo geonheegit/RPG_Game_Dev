c963642435862cac22b5743bfa9409aad6164a65
geonheegit <geonhee625@gmail.com>
2022-07-14 00:19:12 +0900

    도움말 추가 및 미니맵 추가 시도

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ pygame.init()
 pygame.display.set_caption('Adventure & Puzzle')
 clock = pygame.time.Clock()
 font = pygame.font.Font("font/Infinite Darkness.ttf",22)
+kr_font = pygame.font.Font("font/OK CHAN.ttf", 22)
+main_screen_kr_font = pygame.font.Font("font/OK CHAN.ttf", 30)
 
 WIDTH = settings.WIDTH
 HEIGTH = settings.HEIGTH
 FPS = settings.FPS
 
 screen = pygame.display.set_mode((WIDTH, HEIGTH))
+
+minimap_island = pygame.image.load("map/minimap_island.png").convert()
+minimap_on = False
 
 def update_position():
     # 플레이어 위치 표시
@@ -71,8 +76,8 @@ class GameState():
             self.is_beach = False
             self.is_forest = False
 
-        screen.blit(font.render("Press \'ENTER\' to START", 1,
-                                pygame.Color("white")), (WIDTH / 2 - 100, HEIGTH / 2))
+        screen.blit(main_screen_kr_font.render("게임을 시작하려면 엔터를 누르세요.", 0,
+                                pygame.Color("white")), (WIDTH / 4 + 70, HEIGTH / 2))
 
     def island(self):
         for event in pygame.event.get():
@@ -105,8 +110,8 @@ class GameState():
                                                      (level.visible_sprites.map_size[0] * self.zoom, level.visible_sprites.map_size[1] * self.zoom))
             level.visible_sprites.floor_rect = level.visible_sprites.floor_surf.get_rect(topleft=(0, 0))
             # 플레이어 재배치
-            level.player.hitbox.x = 2000
-            level.player.hitbox.y = 2200
+            level.player.hitbox.x = 1500
+            level.player.hitbox.y = 2000
             
             # 맵 변경 변수
             self.is_intro = False
@@ -167,9 +172,9 @@ class GameState():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     # 동굴 x 1900 - 2000, y 2300 - 2400 / 숲 x 2400 - 2500, y 600 - 700 / 해변 x 600 - 800, y 2800 - 2900
-                    if 600 < level.player.hitbox.x < 800 and 2800 < level.player.hitbox.y < 2900:
-                        self.STAGE = 'beach'
-                        level.current_stage = 'beach'
+                    if 600 < level.player.hitbox.x < 800 and 1100 < level.player.hitbox.y < 1200:
+                        self.STAGE = 'island'
+                        level.current_stage = 'island'
 
         if self.is_beach == False:
             # 맵 재로딩
@@ -207,7 +212,7 @@ class GameState():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     # 동굴 x 1900 - 2000, y 2300 - 2400 / 숲 x 2400 - 2500, y 600 - 700 / 해변 x 600 - 800, y 2800 - 2900
-                    if 1900 < level.player.hitbox.x < 2000 and 2300 < level.player.hitbox.y < 2400:  # 숲에서 무인도로 (수정 필요)
+                    if 1700 < level.player.hitbox.x < 1800 and 200 < level.player.hitbox.y < 300:  # 숲에서 무인도로 (수정 필요)
                         self.STAGE = 'island'
                         level.current_stage = 'island'
 
@@ -251,6 +256,54 @@ class GameState():
         if self.STAGE == 'forest':
             self.forest()
 
+    def text_renderer(self):
+        # island에서 각스테이지 진입 도움말 텍스트
+        if self.STAGE == 'island' and level.current_stage == 'island':
+            if 1900 < level.player.hitbox.x < 2000 and 2300 < level.player.hitbox.y < 2400:
+                screen.blit(kr_font.render("동굴에 진입하려면 스페이스바를 누르세요.", 1,
+                                    pygame.Color("black")), (WIDTH / 4 + 70, 100))
+
+            if 600 < level.player.hitbox.x < 800 and 2800 < level.player.hitbox.y < 2900:
+                screen.blit(kr_font.render("해변에 진입하려면 스페이스바를 누르세요.", 1,
+                                           pygame.Color("black")), (WIDTH / 4 + 70, 100))
+
+            if 2400 < level.player.hitbox.x < 2500 and 600 < level.player.hitbox.y < 700:
+                screen.blit(kr_font.render("숲에 진입하려면 스페이스바를 누르세요.", 1,
+                                           pygame.Color("black")), (WIDTH / 4 + 70, 100))
+
+        # cave에서 island로 이동 도움말 텍스트
+        if self.STAGE == 'cave' and level.current_stage == 'cave':
+            if 800 < level.player.hitbox.x < 900 and 3100 < level.player.hitbox.y < 3200:
+                screen.blit(kr_font.render("무인도로 다시 돌아가려면 스페이스바를 누르세요.", 1,
+                                           pygame.Color("black")), (WIDTH / 4 + 70, 100))
+
+        # beach에서 island로 이동 도움말 텍스트
+        if self.STAGE == 'beach' and level.current_stage == 'beach':
+            if 600 < level.player.hitbox.x < 800 and 1100 < level.player.hitbox.y < 1200:
+                screen.blit(kr_font.render("무인도로 다시 돌아가려면 스페이스바를 누르세요.", 1,
+                                           pygame.Color("black")), (WIDTH / 4 + 70, 100))
+
+        # forest에서 island로 이동 도움말 텍스트
+        if self.STAGE == 'forest' and level.current_stage == 'forest':
+            if 1700 < level.player.hitbox.x < 1800 and 200 < level.player.hitbox.y < 300:
+                screen.blit(kr_font.render("무인도로 다시 돌아가려면 스페이스바를 누르세요.", 1,
+                                           pygame.Color("black")), (WIDTH / 4 + 70, 100))
+
+    # def minimap_loader(self):
+    #     global minimap_on
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.KEYDOWN:
+    #             print("qwd")
+    #             if event.key == pygame.K_m:
+    #                 if not minimap_on:
+    #                     minimap_on = True
+    #                 else:
+    #                     minimap_on = False
+
+        if minimap_on:
+            if self.STAGE == 'island' and level.current_stage == 'island':
+                screen.blit(minimap_island, (0, 0))
+
     def bgm_play(self):
         if self.is_cave and not self.cave_bgm_chan.get_busy():
             self.island_bgm_chan.stop()  # 중복 재생 방지
@@ -264,6 +317,8 @@ game_state = GameState()
 
 while True:
     game_state.state_manager()
+    game_state.minimap_loader()
+    game_state.text_renderer()
     clock.tick(FPS)
     pygame.display.update()
     game_state.bgm_play()
