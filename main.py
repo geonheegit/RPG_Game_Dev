1b5d8048ef5be7a971744278237ba4ae7cadd9f3
geonheegit <geonhee625@gmail.com>
2022-07-13 21:27:11 +0900

    해변 양방향 업데이트 텍스처 업데이트

--- main.py
+++ main.py
@@ -182,8 +182,8 @@ class GameState():
                                                                        level.visible_sprites.map_size[1] * self.zoom))
             level.visible_sprites.floor_rect = level.visible_sprites.floor_surf.get_rect(topleft=(0, 0))
             # 플레이어 재배치
-            level.player.hitbox.x = 400
-            level.player.hitbox.y = 1000
+            level.player.hitbox.x = 120
+            level.player.hitbox.y = 1100
 
             # 맵 변경 변수
             self.is_intro = False
